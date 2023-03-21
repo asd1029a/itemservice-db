82390e5a6328520517ae0904a4d4519ef6b5b951
kjm <kimth5533@naver.com>
2023-03-21 22:47:16 +0900

    db2 study 15

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -25,7 +25,7 @@ class ItemRepositoryTest {
 //    @Autowired
 //    PlatformTransactionManager platformTransactionManager;
 //
-//    TransactionStatus status;;
+//    TransactionStatus status;
 
     @BeforeEach
     void beforeEach() {
